4589c16e1e0f39c6518b358b43160194cd985c9d
MarcMarquezM <55757884+MarcMarquezM@users.noreply.github.com>
2020-11-06 17:56:11 -0600

    Add files via upload
    Agregados sensores luz y temperatura al nodemcu y se despliegan en la app de blynk, falta agregar servo del clima y codigo de botones de blynk, al igual que optimizar el codigo.

---
+++ PrototipoNodeMCU4.ino
@@ -0,0 +1,219 @@
+// SENSORES DE LUZ Y TEMPERATURA AGREGADOS
+
+#define BLYNK_PRINT Serial
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <DHT.h>
+
+char auth[] = "92ZcEX4J8U-uP5Dy56GmtyE5KphmnMYp";
+
+// Your WiFi credentials.
+char ssid[] = "INFINITUM1DC6_2.4";
+char pass[] = "Mp83485845";
+
+#include <Servo.h>
+
+Servo servo1;
+Servo servo2;
+
+int pos1 = 0;
+int pos2 = 0;
+
+#define button1 D1
+#define button2 D2
+
+#define LED1 D3
+#define LED2 D4
+
+#define DHTPIN D5
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+// Button State 1
+int buttonState1 = 0;
+int directionState1 = 0;
+
+// Button State 2
+int buttonState2 = 0; 
+int directionState2 = 0;
+
+int pinValue1; // Virtual Button 1 // SERVO 1
+int pinValue2; // Virtual Button 2 // SERVO 2
+
+BLYNK_WRITE(V1) // V1 is the number of Virtual Pin SERVO 1
+{
+  pinValue1 = param.asInt();
+}
+
+BLYNK_WRITE(V2) // V2 is the number of Virtual Pin SERVO 2
+{
+  pinValue2 = param.asInt();
+}
+
+WidgetLED led1(V3); // LED 1
+WidgetLED led2(V4); // LED 2
+
+int lightON = 180;
+int lightOFF = 90;
+
+
+BlynkTimer timer;
+
+void lightSensor() {
+  int photoresistor = analogRead(A0);
+  Blynk.virtualWrite(V5, photoresistor);
+}
+
+void tempSensor() {
+  float t = dht.readTemperature();
+
+  if (isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Blynk.virtualWrite(V9, t);
+}
+
+void setup() {
+  // Debug Console
+  Serial.begin(9600);
+
+  // TEMPERATURE SENSOR
+  dht.begin();
+
+  // SETUP FIRST BUTTON
+  pinMode(button1, INPUT);
+  pinMode(LED1, OUTPUT);
+  servo1.attach(D7);
+ 
+  // SETUP SECOND BUTTON
+  pinMode(button2, INPUT);
+  pinMode(LED2, OUTPUT);
+  servo2.attach(D8);
+
+  // TIMER BLYNK
+  timer.setInterval(500L, lightSensor);
+  timer.setInterval(500L, tempSensor);
+
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop() {
+
+  Blynk.run();
+  timer.run();
+
+  // SERVO 1
+  buttonState1 = digitalRead(button1);
+  if (directionState1 == 0) {
+    if (buttonState1 == HIGH || pinValue1 == 1) {
+      directionState1 = 1;
+      digitalWrite(LED1, HIGH);
+      servo1.attach(D7);
+
+      for (pos1 = lightOFF; pos1 <= lightON; pos1 = pos1 + 1) {
+        servo1.write(pos1);
+        delay(15);
+      }
+
+      //servoMove(pos1, servo1, true);
+
+      servo1.detach();
+      digitalWrite(LED1, LOW);
+    }
+
+  } else if (directionState1 == 1) {
+    if (buttonState1 == HIGH || pinValue1 == 1) {
+      directionState1 = 0;
+      digitalWrite(LED1, HIGH);
+      servo1.attach(D7);
+
+      for (pos1 = lightON; pos1 >= lightOFF; pos1 = pos1 - 1) {
+        servo1.write(pos1);
+        delay(15);
+      }
+
+      //servoMove(pos1, servo1, false);
+
+      servo1.detach();
+      digitalWrite(LED1, LOW);
+    }
+  }
+
+  if (servo1.read() == lightON) { // ESTADO LED 1 EN BLYNK
+    led1.on();
+  } else {
+    led1.off();
+  }
+
+
+
+  // SERVO 2
+  buttonState2 = digitalRead(button2);
+  if (directionState2 == 0) {
+    if (buttonState2 == HIGH || pinValue2 == 1) {
+      directionState2 = 1;
+      digitalWrite(LED2, HIGH);
+      servo2.attach(D8);
+
+      for (pos2 = lightOFF; pos2 <= lightON; pos2 = pos2 + 1) {
+        servo2.write(pos2);
+        delay(15);
+      }
+
+      //servoMove(pos2, servo2, true);
+      
+      servo2.detach();
+      digitalWrite(LED2, LOW);
+    }
+
+  } else if (directionState2 == 1) {
+    if (buttonState2 == HIGH || pinValue2 == 1) {
+      directionState2 = 0;
+      digitalWrite(LED2, HIGH);
+      servo2.attach(D8);
+
+      for (pos2 = lightON; pos2 >= lightOFF; pos2 = pos2 - 1) {
+        servo2.write(pos2);
+        delay(15);
+      }
+
+      //servoMove(pos2, servo2, false);
+      
+      servo2.detach();
+      digitalWrite(LED2, LOW);
+    }
+  }
+
+  if (servo2.read() == lightON) { // ESTADO LED 2 EN BLYNK
+    led2.on();
+  } else {
+    led2.off();
+  }
+  
+} // EN OF VOID LOOP
+
+
+
+void servoMove(int pos, Servo servo, bool OnOff) {
+
+  if (OnOff == true) {
+
+    for (pos = 90; pos <= 180; pos = pos + 1) {
+      servo.write(pos);
+      delay(15);
+    } 
+
+  } else if (OnOff == false) {
+    
+    for (pos = 180; pos >= 90; pos = pos - 1) {
+      servo.write(pos);
+      delay(15);
+    }
+    
+  } // END ELSE
+  
+} // END FUNCTION
